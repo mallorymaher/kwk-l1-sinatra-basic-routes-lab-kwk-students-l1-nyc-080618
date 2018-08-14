3ba6cfe1efe71df803de4d035dbec12f7e9da630
mallorymaher <mallory.maher@comcast.net>
2018-08-14 15:26:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,32 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'Welcome to my page!'
+  end
+  
+  get '/name' do
+    'Hi! My name is Mal!'
+  end
+  
+  get '/hometown' do
+    'I was born and raised in New Market, MD.'
+  end
+  
+  get '/song' do
+    'My favorite songs are Haunted and King of My Heart by Taylor Swift'
+  end
+  
+  get '/food' do
+    'My favorite food is fried chicken.'
+  end
+  
+  get '/color' do
+    'My favorite color is lilac.'
+  end
+  
+  get '/subject' do
+    'My favorite subject in school is computer science.'
+  end
+    
 end
