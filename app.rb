588d444e1cd6861874df2706e559813660a4aa92
mallorymaher <mallory.maher@comcast.net>
2018-08-14 15:26:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,4 +9,24 @@ class App < Sinatra::Base
     'Hi! My name is Mal!'
   end
   
+  get '/hometown' do
+    'I was born and raised in New Market, MD.'
+  end
+  
+  get '/song' do
+    'My favorite songs are Haunted and King of My Heart by Taylor Swift'
+  end
+  
+  get '/food' do
+    'My favorite food is fried chicken.'
+  end
+  
+  get '/color' do
+    'My favorite color is lilac.'
+  end
+  
+  get '/subject' do
+    'My favorite subject in school is computer science.'
+  end
+    
 end
